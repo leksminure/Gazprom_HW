e7ef412c57d5390c6efa7f26be459a026ac4d20d
leksminure <91364694+leksminure@users.noreply.github.com>
2023-01-06 22:00:35 +0300

    added hw-2

---
+++ HW-2/2.1.py
@@ -0,0 +1,37 @@
+def psp(n: int) -> str:
+    """
+    Описание:
+    Функция возвращает все скобочные последовательности в лексикографическом порядке при n открвающих скобках
+    """
+    def next_sequence(sequence: str) -> str:
+        """
+        Описание:
+        Функция принимает на вход скобочную последовательность, возвращает следующую скобочную последовательность, в
+        соответствии с лексикографическим порядком, если скобочная последовательность являлась последней, функция
+        вернет 'No next sequence'
+        """
+        count_opened = 0
+        count_closed = 0
+        for bracket in reversed(sequence):
+            if bracket == ')':
+                count_closed += 1
+            else:
+                count_opened += 1
+                if count_opened < count_closed:
+                    break
+        sequence = sequence[:len(sequence)-count_closed-count_opened]
+        if not sequence:
+            return 'No next sequence'
+        sequence += ')'
+        for i in range(count_opened):
+            sequence += '('
+        for i in range(count_closed-1):
+            sequence += ')'
+        return sequence
+
+    result = ''
+    sequence = '(' * n + ')' * n
+    while sequence != 'No next sequence':
+        result += sequence + '\n'
+        sequence = next_sequence(sequence)
+    return result.rstrip()
---
+++ HW-2/2.2.py
@@ -0,0 +1,43 @@
+from functools import reduce
+
+
+def largest_number(n: int, numbers: str) -> str:
+    """
+    Описание:
+    В функции совершается проход по всем числам из входной строки и для каждого считается сумма
+    Далее формируется список кортежей из числа и его суммы и сортируется по второму элементу, то есть его сумме
+    Как итог происходит конкатенация всех чисел, являющихся строками и первыми элементами кортежей
+
+    Параметры
+    ----------
+    n : число чисел, содержащихся во входной строке
+    numbers: строка с n числами, записанными через пробел. Каждое отдельное число не превосходит 1000
+    ----------
+    Возвращаемые значения
+    ----------
+    result: строка, являющаяся конкатенацией и максимальным числом при данных входныхых числах
+    """
+    def sum_of_digits(number):
+        """
+        Описание:
+        Функция принимает на вход число, возвращает сумму его отдельно взятых цифр
+        """
+        result = 0
+        while number != 0:
+            last_digit = number % 10
+            result += last_digit
+            number //= 10
+        return result
+
+    if n == 0:
+        return ''
+    numbers_sums = []
+    numbers = numbers.split()
+    for i in range(n):
+        numbers_sums.append(
+            (numbers[i], sum_of_digits(int(numbers[i])))
+        )
+    numbers_sums = sorted(numbers_sums, key=lambda x: x[1], reverse=True)
+    numbers = [x[0] for x in numbers_sums]
+    result = reduce(lambda x, y: x+y, numbers)
+    return result
